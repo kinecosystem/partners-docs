1737f4cd68a1f4974c509508e7496d8f52b9912c
nitzantomer <nitzan.tomer@gmail.com>
2018-12-17 11:45:46 +0200

    removed the FAQ section

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -27,7 +27,7 @@ const siteConfig = {
 	// For no header links in the top nav bar -> headerLinks: [],
 	headerLinks: [
 		{ doc: "android/android", label: "Docs" },
-		{ doc: "faq/integration", label: "FAQ" },
+		// { doc: "faq/integration", label: "FAQ" },
 		{ page: "help", label: "Help" }
 	],
 
